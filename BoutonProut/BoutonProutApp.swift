688b515e1124cae0c0f661993c831a67e721c939
Paul Serra <ps.paulserra@gmail.com>
2025-12-14 14:44:06 +0100

    Initial Commit

---
+++ BoutonProut/BoutonProutApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoutonProutApp.swift
+//  BoutonProut
+//
+//  Created by Paul Serra on 14/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct BoutonProutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
